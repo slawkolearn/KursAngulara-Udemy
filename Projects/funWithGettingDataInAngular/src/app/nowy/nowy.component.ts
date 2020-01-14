70d8aaf1da6904d12a6ad63ba8fe524790035bc6
Sławomir Lasik <slawomir.lasik@gmail.com>
2020-01-15 00:05:00 +0100

    More options for ngFor* - see the funWithGettingDataInAngular

---
+++ Projects/funWithGettingDataInAngular/src/app/nowy/nowy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nowy',
+  templateUrl: './nowy.component.html',
+  styleUrls: ['./nowy.component.css']
+})
+export class NowyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
